a9573591c95b6c3e2537989740b159d6b49fa492
harness17 <harnesswinner@gmail.com>
2026-04-18 20:19:18 +0900

    feat: add SQLite connection module with WAL mode

---
+++ src/main/db/connection.js
@@ -0,0 +1,12 @@
+import Database from 'better-sqlite3'
+
+export function openDatabase(path) {
+  const db = new Database(path)
+  db.pragma('journal_mode = WAL')
+  db.pragma('foreign_keys = ON')
+  return db
+}
+
+export function closeDatabase(db) {
+  if (db && db.open) db.close()
+}
